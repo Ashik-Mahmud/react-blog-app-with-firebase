7bed732b1c547471b396e02867a3f1960c49d48c
ashikmahmud <ashikmamud187@gmail.com>
2022-04-05 05:59:48 +0600

    initilization react-blog-app using firebase projects

---
+++ src/components/Chart.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  Area,
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const Chart = () => {
+  const data = [
+    {
+      name: "Page A",
+      uv: 590,
+      pv: 800,
+      amt: 1400,
+    },
+    {
+      name: "Page B",
+      uv: 868,
+      pv: 967,
+      amt: 1506,
+    },
+    {
+      name: "Page C",
+      uv: 1397,
+      pv: 1098,
+      amt: 989,
+    },
+    {
+      name: "Page D",
+      uv: 1480,
+      pv: 1200,
+      amt: 1228,
+    },
+    {
+      name: "Page E",
+      uv: 1520,
+      pv: 1108,
+      amt: 1100,
+    },
+    {
+      name: "Page F",
+      uv: 1400,
+      pv: 680,
+      amt: 1700,
+    },
+  ];
+  return (
+    <div className="chart">
+      <div className="container">
+        <h1 className="mx-2 text-center colorize">Analytics </h1>
+        <div style={{ width: 1260, height: 300 }}>
+          <ResponsiveContainer>
+            <ComposedChart
+              width={500}
+              height={400}
+              data={data}
+              margin={{
+                top: 20,
+                right: 20,
+                bottom: 20,
+                left: 20,
+              }}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Area
+                type="monotone"
+                dataKey="amt"
+                fill="#8884d8"
+                stroke="#8884d8"
+              />
+              <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+              <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="line-chart" style={{ width: 1260, height: 300 }}>
+          <ResponsiveContainer>
+            <LineChart
+              width={500}
+              height={200}
+              data={data}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Line
+                connectNulls
+                type="monotone"
+                dataKey="uv"
+                stroke="#8884d8"
+                fill="#8884d8"
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
